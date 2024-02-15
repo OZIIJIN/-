2b1a20da3eb7395a9826a03a1f9ba8dca2953fbd
OZIIJIN <dpwlsdlqma6@gmail.com>
2024-02-15 10:28:50 +0900

    프로그래머스 Lv.2

--- 피보나치 수.java
+++ 피보나치 수.java
@@ -7,7 +7,7 @@ class Solution {
         arr[1] = 1;
         
         for(int i = 2; i <= n; i++){
-            arr[i] = (arr[i-1] + arr[i-2])%1234567;
+            arr[i] = (arr[i-1]%1234567 + arr[i-2]%1234567);
         }
         
         answer = arr[n]%1234567;
