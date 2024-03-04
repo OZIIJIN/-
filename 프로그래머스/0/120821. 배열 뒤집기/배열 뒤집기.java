11b632c459592351ef581b53434f8f4e4ef278be
OZIIJIN <dpwlsdlqma6@gmail.com>
2024-03-04 20:59:46 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 84.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+        int start = 0;
+        
+        for(int i = num_list.length - 1; i >= 0; i--){
+            answer[start] = num_list[i];
+            start++;
+        } 
+    
+        return answer;
+    }
+}
