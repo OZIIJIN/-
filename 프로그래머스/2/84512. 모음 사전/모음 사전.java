13e9ea7734a3f3b601257ce0918b1a33e2002b74
OZIIJIN <dpwlsdlqma6@gmail.com>
2024-03-21 10:09:31 +0900

    [level 2] Title: 모음 사전, Time: 0.12 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,20 +1,7 @@
-import java.util.*;
 class Solution {
     public int solution(String word) {
         int answer = 0;
-        int length = word.length();
-        int[] nums = {781, 156, 31, 6, 1};
-        Map<String, Integer> map = new HashMap<>();
-        map.put("A", 0);
-        map.put("E", 1);
-        map.put("I", 2);
-        map.put("O", 3);
-        map.put("U", 4);
-        
-        String[] words = word.split("");
-        for(int i = 0; i < words.length; i++) {
-            answer += 1 + map.get(words[i])*nums[i];
-        }
+        int num = 
         
         return answer;
     }
