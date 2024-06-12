5beee1ceabe7d39eb0c460f331b62fe19d22d440
OZIIJIN <dpwlsdlqma6@gmail.com>
2024-06-12 23:35:18 +0900

    [level 0] Title: 가위 바위 보, Time: 0.03 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String rsp) {
+        
+        char [] arr = rsp.toCharArray();
+        
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == '2') {
+                arr[i] = '0';
+            } else if (arr[i] == '0') {
+                arr[i] = '5';
+            } else {
+                arr[i] = '2';
+            }
+        }
+        
+        String answer = String.valueOf(arr);
+        return answer;
+    }
+}
