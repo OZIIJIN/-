d8ce2add29d8fa26b82b7f7c212f96b4fc755b9c
OZIIJIN <dpwlsdlqma6@gmail.com>
2024-10-24 09:24:59 +0900

    [level 2] Title: 의상, Time: 0.09 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,23 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(String [] c: clothes) {
+            map.put(c[1], map.getOrDefault(c[1], 0) +1);
+        }
+        
         int answer = 1;
-        
-        //종류별 의상 HashMap에 저장
-        Map<String, Integer> clothesMap = new HashMap<>();
-        
-        for(int i = 0; i < clothes.length; i++){
-            String clothingType = clothes[i][1];
-            clothesMap.put(clothingType, clothesMap.getOrDefault(clothingType, 0) + 1);
+        for(String s: map.keySet()) {
+            answer *= (map.get(s) +1);
         }
-        
-         for(String s : clothesMap.keySet()) {
-            answer *= (clothesMap.get(s) + 1);   // 조합 -> 안입는 경우도 고려하기 위해 + 1
-        }
-        
-        answer -= 1; // 모두 '안입음'일 경우 -1 해주기 
-        
-        
-        return answer;
+        return answer-1;
     }
 }
